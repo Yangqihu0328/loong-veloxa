480e9a9679463e40a7a009679672d8c18a5c43c2
qihooz <187205342@qq.com>
2026-04-05 16:24:25 +0800

    feat(graphics): Canvas and Path pure virtual interfaces
    Canvas: fill/stroke/clip/layer/transform/state/path factory.
    Path: moveto/lineto/quadto/cubicto/arcto/close/bounds/contains.

--- veloxa/graphics/canvas.h
+++ veloxa/graphics/canvas.h
@@ -1,9 +1,51 @@
 #ifndef VELOXA_GRAPHICS_CANVAS_H_
 #define VELOXA_GRAPHICS_CANVAS_H_
 
+#include <memory>
+
+#include "veloxa/graphics/brush.h"
+#include "veloxa/graphics/path.h"
 #include "veloxa/graphics/types.h"
 
 namespace vx::gfx {
+
+class Canvas {
+ public:
+  virtual ~Canvas() = default;
+
+  virtual void Begin() = 0;
+  virtual void End() = 0;
+  virtual void Clear(Color color) = 0;
+
+  virtual void FillRect(const Rect& rect, const Brush& brush) = 0;
+  virtual void FillRoundedRect(const Rect& rect, vx::f32 radius,
+                               const Brush& brush) = 0;
+  virtual void FillPath(const Path& path, const Brush& brush) = 0;
+
+  virtual void StrokeRect(const Rect& rect, const Brush& brush,
+                          vx::f32 width) = 0;
+  virtual void StrokeRoundedRect(const Rect& rect, vx::f32 radius,
+                                 const Brush& brush, vx::f32 width) = 0;
+  virtual void StrokePath(const Path& path, const Brush& brush,
+                          vx::f32 width) = 0;
+  virtual void StrokeLine(Point a, Point b, const Brush& brush,
+                          vx::f32 width) = 0;
+
+  virtual void PushClipRect(const Rect& rect) = 0;
+  virtual void PushClipPath(const Path& path) = 0;
+  virtual void PopClip() = 0;
+
+  virtual void PushLayer(const Rect& bounds, vx::f32 opacity) = 0;
+  virtual void PopLayer() = 0;
+
+  virtual void SetTransform(const Matrix3x2& m) = 0;
+  virtual Matrix3x2 GetTransform() const = 0;
+  virtual void PushState() = 0;
+  virtual void PopState() = 0;
+
+  virtual std::unique_ptr<Path> CreatePath() = 0;
+};
+
 }  // namespace vx::gfx
 
 #endif  // VELOXA_GRAPHICS_CANVAS_H_
--- veloxa/graphics/path.h
+++ veloxa/graphics/path.h
@@ -4,6 +4,25 @@
 #include "veloxa/graphics/types.h"
 
 namespace vx::gfx {
+
+class Path {
+ public:
+  virtual ~Path() = default;
+
+  virtual void MoveTo(Point p) = 0;
+  virtual void LineTo(Point p) = 0;
+  virtual void QuadTo(Point control, Point end) = 0;
+  virtual void CubicTo(Point c1, Point c2, Point end) = 0;
+  virtual void ArcTo(Point center, vx::f32 radius, vx::f32 start_angle,
+                     vx::f32 sweep) = 0;
+  virtual void Close() = 0;
+  virtual void Reset() = 0;
+
+  virtual bool IsEmpty() const = 0;
+  virtual Rect Bounds() const = 0;
+  virtual bool Contains(Point p) const = 0;
+};
+
 }  // namespace vx::gfx
 
 #endif  // VELOXA_GRAPHICS_PATH_H_
